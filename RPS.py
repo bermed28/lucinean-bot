c2421cafe4ab6f87964b7f35da0a359ccb09d7e4
Fernando J. Bermúdez Medina <fernando.bermudez1@upr.edu>
2020-08-14 21:19:58 -0400

    Added Coding Session Features

---
+++ RPS.py
@@ -0,0 +1,45 @@
+import discord
+from discord import client
+from random import randint as randomNumber
+
+weapons = ["paper","scissors","rock"]
+
+#(index+1) % len(list)
+async def rockPaperScissors(client : discord.Client, message : discord.Message):
+    userWins = 0
+    botWins = 0
+    while True:
+        if userWins == 2 or botWins == 2:
+            break
+
+        await message.channel.send("Choose your weapon: rock/paper/scissors")
+        response = await client.wait_for("message", check=lambda response_message: response_message.author == message.author)
+        botChoice = randomNumber(0, 2)
+        await message.channel.send(weapons[botChoice])
+        index = -1
+        for i in range(len(weapons)):
+            if response.content == weapons[i]:
+                index = i
+                break
+
+        if index == (botChoice + 1) % len(weapons):
+            userWins += 1
+            await message.channel.send(f"{message.author.nick} wins: {userWins}, Bot Wins: {botWins}")
+        elif botChoice == (index + 1) % len(weapons):
+            botWins += 1
+            await message.channel.send(f"{message.author.nick} wins: {userWins}, Bot Wins: {botWins}")
+        else:
+            await message.channel.send("It was a tie :(")
+
+    if userWins == 2:
+        await message.channel.send(f"{message.author.nick} wins!")
+    else:
+        await message.channel.send(f"I win!")
+
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -16,11 +16,24 @@ from datetime import datetime
 import encrypt_token as et
 
 import discord
-
+import RPS
 import bot
 import log
 
 client = discord.Client()
+guild = client.get_guild(718624993470316554)
+_PLAYING = False
+battle_cries = {
+        731572939434098691 : "https://tenor.com/view/smashbros-lucina-gif-14611011", #Lucina
+        731573113325879407 : "https://i.gifer.com/777O.gif", #Wario
+        731573203411140679 : "https://tenor.com/view/fox-smash-bros-come-at-me-gif-13203706", #Fox
+        731573284474454117 : "https://tenor.com/baz34.gif", #Peach
+        731573154467807364 : "https://tenor.com/view/super-smash-bros-for-wii-u-princess-rosalina-luma-rosalina-and-luma-video-game-gif-17524513", #Rosalina
+        731573039082373160 : "https://giphy.com/gifs/holmes-professor-chulock-qzDuPFbEYiEVy", #Pikachu
+        731572838640910431 : "https://tenor.com/view/zelda-the-legend-of-zelda-link-master-sword-ocarina-of-time-gif-5833491", #Link
+        731572994413166723 : "https://tenor.com/view/super-smash-bros-ultimate-zelda-gif-12180489", #Zelda
+        718630411093278783 : "https://tenor.com/view/super-smash-brothers-super-smash-bros-nintendo-switch-2018-gif-11303168" #Smash
+        }
 
 
 async def task():
@@ -67,8 +80,30 @@ while True:
         if message.content == "!marco":
             await message.channel.send("Polo! :)")
 
-        if message.content == "!pong":
-            await message.channel.send("Ping! :)")
+        if message.content == "!team_gif":
+
+            """
+            loop thru al the roles
+            if the role matches a certain group, send the cry of that group
+            """
+
+            for role in message.author.roles: #goes thru all roles of memeber
+                for id, cry in battle_cries.items():
+                    if role.id == id:
+                        log.debug("[CRY] ACTIVATED GIF")
+                        await message.channel.send(battle_cries[id])
+                        return
+        if message.content == "roles":
+            log.debug(message.author.roles)
+
+        if message.content == "!rps":
+            global _PLAYING
+            if not _PLAYING:
+                _PLAYING = True
+                await RPS.rockPaperScissors(client,message)
+                _PLAYING = False
+            else:
+                return
 
         log.debug('[INFO] passed the filter')
 
